caf6c3184a319bf039bb0ee0fd8d8bf4a217e470
Ciro Duran <ciro.duran@gmail.com>
2013-12-18 17:27:11 +0000

    Added minimal Binary Space Partition generator. Compiles but there are problems with SSE calculations in vec4. Still not tested completely.

--- src/examples/layer2/engine.h
+++ src/examples/layer2/engine.h
@@ -18,73 +18,25 @@ namespace octet {
     // shaders to draw triangles
     bump_shader object_shader;
     bump_shader skin_shader;
+    color_shader cshader;
 
     // helper to rotate camera about scene
     mouse_ball ball;
 
-    // helper for drawing text
-    text_overlay overlay;
-
-    // helper for debugging by web browser
-    http_server server;
-
     // helper for picking objects on the screen
     object_picker picker;
 
-    // test c/c++ parser
-    //cpp_parser parser;
+    mat4t cameraToWorld;
 
-    void load_file(const char *filename) {
-      FILE *file = fopen(app_utils::get_path(filename), "rb");
-      char buf[8];
-      scene *app_scene = 0;
-      if (file && fread(buf, 1, sizeof(buf), file) && !memcmp(buf, "octet", 5)) {
-        fseek(file, 0, SEEK_SET);
-        binary_reader r(file);
-        dict.visit(r);
-        fclose(file);
-        app_scene = dict.get_active_scene();
-      } else {
-        if (file) fclose(file);
-        collada_builder builder;
-        if (!builder.load_xml(filename)) {
-          printf("\nERROR: could not open %s\nThis is likely a problem with paths.", filename);
-          return;
-        }
+    City *city;
 
-        builder.get_resources(dict);
-        app_scene = dict.get_scene(builder.get_default_scene());
-
-        assert(app_scene);
-
-        app_scene->create_default_camera_and_lights();
-
-        dict.set_active_scene(app_scene);
-      }
-
-      app_scene->play_all_anims(dict);
-
-      for (unsigned i = 0; i != app_scene->get_num_mesh_instances(); ++i) {
-        mesh_instance *mi = app_scene->get_mesh_instance(i);
-        //mi->set_mesh(new wireframe(new displacement_map(mi->get_mesh())));
-        //mi->set_mesh(new smooth(mi->get_mesh()));
-        //mi->set_mesh(mi->get_mesh());
-        mi->set_mesh(new indexer(mi->get_mesh()));
-      }
-
-      if (app_scene->get_num_camera_instances() != 0) {
-        camera_instance *cam = app_scene->get_camera_instance(0);
-        scene_node *node = cam->get_node();
-        mat4t cameraToWorld = node->get_nodeToParent();
-        ball.init(this, cameraToWorld.w().length(), 360.0f);
-      }
-    }
   public:
     // this is called when we construct the class
-    engine(int argc, char **argv) : app(argc, argv), ball() {
-
-      // test the c++ parser
-      //parser.parse("int x = 1;");
+    engine(int argc, char **argv) 
+    : app(argc, argv)
+    , ball()
+    , cameraToWorld()
+    {
     }
 
     // this is called once OpenGL is initialized
@@ -92,25 +44,12 @@ namespace octet {
       // set up the shaders
       object_shader.init(false);
       skin_shader.init(true);
+      cshader.init();
 
-      const char *filename = 0;
+      srand(NULL);
 
-      int selector = 0;
-      switch (selector) {
-        case 0: filename = "assets/duck_triangulate.dae"; break;
-        case 1: filename = "assets/skinning/skin_unrot.dae"; break;
-        case 2: filename = "assets/jenga.dae"; break;
-        case 3: filename = "assets/duck_ambient.dae"; break;
-        case 4: filename = "assets/Laurana50k.dae"; break;
-        case 5: filename = "external/Arteria3d/ElvenMale/ElevenMaleKnight_blender.dae"; break;
-        case 6: filename = "external/Arteria3d/arteria3d_tropicalpack/flowers/flower%202.dae"; break; 
-        case 7: filename = "assets/plane.dae"; break;
-      }
+      city = City::createFromRectangle(5.0f, 5.0f);
 
-      load_file(filename);
-
-      overlay.init();
-      server.init(&dict);
       picker.init(this);
     }
 
@@ -126,72 +65,15 @@ namespace octet {
       // allow Z buffer depth testing (closer objects are always drawn in front of far ones)
       glEnable(GL_DEPTH_TEST);
 
-      GLint param;
-      glGetIntegerv(GL_SAMPLE_BUFFERS, &param);
-      if (param == 0) {
-        // if multisampling is disabled, we can't use GL_SAMPLE_COVERAGE (which I think is mean)
-        // Instead, allow alpha blend (transparency when alpha channel is 0)
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+      int vx = 0, vy = 0;
+      get_viewport_size(vx, vy);
 
-        // this is a rather brutal alpha test that cuts off anything with a small alpha.
-        #ifndef SN_TARGET_PSP2
-          glEnable(GL_ALPHA_TEST);
-          glAlphaFunc(GL_GREATER, 0.9f);
-        #endif
-      } else {
-        // if multisampling is enabled, use GL_SAMPLE_COVERAGE instead
-        glEnable(GL_SAMPLE_ALPHA_TO_COVERAGE);
-        glEnable(GL_SAMPLE_COVERAGE);
-      }
+      cameraToWorld.loadIdentity();
+      cameraToWorld.translate(0.0f, 0.5f, 1.0f);
+      ball.update(cameraToWorld);
+      //picker.update(app_scene);
 
-      // poll web server
-      server.update();
-
-      // ctrl-s: save file
-      if (is_key_down('S') && is_key_down(key_ctrl)) {
-        FILE *file = fopen("c:/tmp/save.oct", "wb");
-        binary_writer b(file);
-        dict.visit(b);
-        fclose(file);
-
-        /*resources loaded;
-        file = fopen("save.oct", "rb");
-        binary_reader r(file);
-        loaded.visit(r);
-        fclose(file);*/
-
-      }
-
-      // drag and drop file loading
-      dynarray<string> &queue = access_load_queue();
-      if (queue.size()) {
-        // replace scene
-        dict.reset();
-        string url;
-        url.urlencode(queue[0]);
-        load_file(url);
-        queue.resize(0);
-      }
-
-      scene *app_scene = dict.get_active_scene();
-      if (app_scene && app_scene->get_num_camera_instances()) {
-        int vx = 0, vy = 0;
-        get_viewport_size(vx, vy);
-
-        camera_instance *cam = app_scene->get_camera_instance(0);
-        scene_node *node = cam->get_node();
-        mat4t &cameraToWorld = node->access_nodeToParent();
-        ball.update(cameraToWorld);
-        picker.update(app_scene);
-
-        // update matrices. assume 30 fps.
-        app_scene->update(1.0f/30);
-
-        app_scene->render(object_shader, skin_shader, *cam, (float)vx / vy);
-
-        overlay.render(object_shader, skin_shader, vx, vy, get_frame_number());
-      }
+      city->debugRender(cshader, cameraToWorld, float(vx)/float(vy), 1);
     }
   };
 }
--- src/nntcity/cityobjs.h
+++ src/nntcity/cityobjs.h
@@ -1,23 +1,26 @@
 namespace octet {
 
-  class BSPRoot {
+  class BSPNode {
   public:
     vec4 vertices[4];
-    BSPRoot *left;
-    BSPRoot *right;
-  };
+    BSPNode *left;
+    BSPNode *right;
 
-  class BSPNode : public BSPRoot {
-    //Indicates which vertices are common to other partition
-    int common_indices[2]; 
+    BSPNode()
+    : left(NULL)
+    , right(NULL)
+    {
+      
+    }
   };
 
   class City {
   public:
-    BSPRoot root;
+    BSPNode root;
 
-    City () {
-    }
+    mat4t modelToWorld;
+
+    City () {}
 
     static City *createFromRectangle(float width, float height) {
       vec4 vert_[4];
@@ -38,16 +41,107 @@ namespace octet {
       root.vertices[1] = *(vertices_+1);
       root.vertices[2] = *(vertices_+2);
       root.vertices[3] = *(vertices_+3);
+      root.left = NULL;
+      root.right = NULL;
+      modelToWorld.loadIdentity();
     }
 
-    void stepPartition(/* camera frustrum */) {
-      
+    void stepPartition(unsigned int depth/* camera frustrum */) {
+      stepPartition_(depth, &root);
     }
 
     void iterator() {
 
     }
-  };
 
-  
+    void debugRender(color_shader &s, mat4t &cameraToWorld, float aspectRatio, unsigned int depth) {
+      debugRenderRect_(s, cameraToWorld, aspectRatio, depth, &root);
+    }
+
+    private:
+
+    void debugRenderRect_(color_shader &s, mat4t &cameraToWorld, float aspectRatio, unsigned int depth, BSPNode *node) {
+      if (depth == 0) return;
+      if (!node) return;
+
+      mat4t modelToProjection = mat4t::build_projection_matrix(modelToWorld, cameraToWorld);
+
+      s.render(modelToProjection, vec4(1.0f, 0.0f, 0.0f));
+
+      float vertices[] = {
+        node->vertices[0].x(), node->vertices[0].y(), node->vertices[0].z(),  
+        node->vertices[1].x(), node->vertices[1].y(), node->vertices[1].z(),  
+        node->vertices[2].x(), node->vertices[2].y(), node->vertices[2].z(),  
+        node->vertices[3].x(), node->vertices[3].y(), node->vertices[3].z()
+      };
+
+      glVertexAttribPointer(attribute_pos, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)vertices);
+      glEnableVertexAttribArray(attribute_pos);
+
+      glDrawArrays(GL_LINE_LOOP, 0, 4);
+
+      debugRenderRect_(s, cameraToWorld, aspectRatio, depth-1, node->left);
+      debugRenderRect_(s, cameraToWorld, aspectRatio, depth-1, node->right);
+    }
+
+    void stepPartition_(unsigned int depth, BSPNode *b) {
+      if (depth == 0) return;
+
+      if (!b->right || !b->left) { // It was a leaf node, expand it
+        //Take longest side, and connect it to opposite side
+        float side_length = (b->vertices[1] - b->vertices[0]).length();
+        int side_index = 0;
+
+        for (int i = 1; i < 3; i++) {
+          float side_length_2 = (b->vertices[i+1] - b->vertices[i]).length();
+          
+          if (side_length_2 > side_length) {
+            side_length = side_length_2;
+            side_index = i;
+          }
+        }
+
+        //Opposite side
+        int opposite_side_index = (side_index+2)%4;
+        vec4 &opposite_side_vertex_a = b->vertices[opposite_side_index];
+        vec4 &opposite_side_vertex_b = b->vertices[(opposite_side_index+1)%4];
+
+        vec4 &side_vertex_a = b->vertices[side_index];
+        vec4 &side_vertex_b = b->vertices[(side_index+1)%4];
+
+        //Calculate mid-points
+        vec4 midpoint(side_vertex_a.x() + (side_vertex_b.x() - side_vertex_a.x())*0.5f,
+                      side_vertex_a.y() + (side_vertex_b.y() - side_vertex_a.y())*0.5f,
+                      side_vertex_a.z() + (side_vertex_b.z() - side_vertex_a.z())*0.5f,
+                      1.0f);
+
+        vec4 midpoint_opposite(side_vertex_a.x() + (side_vertex_b.x() - side_vertex_a.x())*0.5f,
+                      side_vertex_a.y() + (side_vertex_b.y() - side_vertex_a.y())*0.5f,
+                      side_vertex_a.z() + (side_vertex_b.z() - side_vertex_a.z())*0.5f,
+                      1.0f);
+
+        b->left = new BSPNode();
+        b->right = new BSPNode();
+
+        //Put resulting vertices in the same positions as its parent
+        b->left->vertices[0] = side_vertex_a;
+        b->left->vertices[1] = midpoint;
+        b->left->vertices[2] = midpoint_opposite;
+        b->left->vertices[3] = opposite_side_vertex_b;
+
+        b->right->vertices[0] = opposite_side_vertex_a;
+        b->right->vertices[1] = midpoint_opposite;
+        b->right->vertices[2] = midpoint;
+        b->right->vertices[3] = side_vertex_b;
+
+      }
+
+      // Heuristic to choose side: random by now
+      // TODO Heuristic: choose sides intersected by frustrum
+      float r = float(rand())/RAND_MAX;
+      BSPNode *child = (r < 0.5f)? b->left: b->right;
+
+      stepPartition_(depth - 1, child);
+    }
+  }; 
 }
