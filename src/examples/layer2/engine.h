af411a9769a35545132717c0dcd5c33423149026
scual <scual.project@gmail.com>
2014-02-22 16:40:21 +0000

    city_building_bump_shader

--- src/examples/layer2/engine.h
+++ src/examples/layer2/engine.h
@@ -20,6 +20,7 @@ namespace octet {
 
     // shaders to draw triangles
     city_bump_shader city_bump_shader_;
+	city_buildings_bump_shader city_buildings_bump_shader_; 
     bump_shader object_shader;
     color_shader cshader;
 
@@ -79,6 +80,7 @@ namespace octet {
       // Shader Set Up
       //city_bump_shader_.init(); // false is default
       object_shader.init(false);
+	  city_buildings_bump_shader_.init();
   
       cshader.init();
       compassCard.init(&cshader);
@@ -384,7 +386,7 @@ namespace octet {
 
       light_uniforms_array[2] = vec4(sin(light_rotation[0]*3.1415926f/180.0f), sin(light_rotation[1]*3.1415926f/180.0f), cos(light_rotation[0]*3.1415926f/180.0f), 0.0f) * worldToCamera;
 
-      city_mesh->debugRender(object_shader, cshader, modelToProjection, modelToCamera, light_uniforms_array, num_light_uniforms, num_lights, buildingAreaList, drawFlags);
+      city_mesh->debugRender(object_shader, city_buildings_bump_shader_, cshader, modelToProjection, modelToCamera, light_uniforms_array, num_light_uniforms, num_lights, buildingAreaList, drawFlags);
       //city_mesh->debugRender_newShader(streetList, city_bump_shader_, object_shader, modelToProjection, modelToCamera, light_uniforms_array, num_light_uniforms, num_lights);
       //city->debugRender(&cshader, &cameraToWorld, float(vx)/float(vy), depth);
 
